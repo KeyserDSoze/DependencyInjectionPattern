e8e21be1af43c6b2224e3000dca0af7c389d881b
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-12-08 15:23:37 +0100

    beta-1.0.0

---
+++ DependencyInjectionPattern.Interfaces/Log/ILog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionPattern.Interfaces.Log
+{
+    public interface ILog
+    {
+        void Save();
+    }
+}
---
+++ DependencyInjectionPattern.Providers/Log/TableStorageLog.cs
@@ -0,0 +1,15 @@
+﻿using DependencyInjectionPattern.Interfaces.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionPattern.Providers.Log
+{
+    public class TableStorageLog : ILog
+    {
+        public void Save()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+}
